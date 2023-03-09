0377f38b9b8991ca9d9929ffc8efe8166fb51d1d
Margalit-Abermad <2329767+0@gmail.com>
2023-03-09 23:45:47 +0200

    In the meantime, doing everything in one line...

---
+++ IronOCR2/Program.cs
@@ -0,0 +1,6 @@
+﻿
+// PM> Install-Package IronOcr
+using IronOcr;
+//OcrResult result = new IronTesseract().Read(@"C:\Users\user\Downloads\pic1.jpg");
+OcrResult result = new IronTesseract().Read(@"C:\Users\user\Downloads\unnamed (33).png");
+Console.WriteLine(result.Text);
